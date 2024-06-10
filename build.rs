aeabf6e4e7c60351a01898c5b58d9fdd7c9df76c
LeadRDRK <leadrdrk@zohomail.com>
2024-06-10 15:22:22 +0700

    [Windows] Add version information to DLL

--- build.rs
+++ build.rs
@@ -4,5 +4,9 @@ fn main() {
         // Link proxy export defs
         let absolute_path = std::fs::canonicalize("src/windows/proxy/exports.def").unwrap();
         println!("cargo:rustc-cdylib-link-arg=/DEF:{}", absolute_path.display());
+
+        // Generate and link version information
+        let res = tauri_winres::WindowsResource::new();
+        res.compile().unwrap();
     }
 }
