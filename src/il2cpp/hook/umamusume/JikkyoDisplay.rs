9e739cf6f567f2f9573fc30226f3ab1e9352b141
LeadRDRK <leadrdrk@zohomail.com>
2024-08-21 14:29:13 +0700

    Implement template evaluation for race jikkyou display

--- src/il2cpp/hook/umamusume/JikkyoDisplay.rs
+++ src/il2cpp/hook/umamusume/JikkyoDisplay.rs
@@ -1,4 +1,7 @@
-use crate::{core::utils::wrap_text_il2cpp, il2cpp::{symbols::get_method_addr, types::*}};
+use crate::{
+    core::{ext::StringExt, utils::{wrap_text, wrap_text_il2cpp}, Hachimi},
+    il2cpp::{symbols::get_method_addr, types::*}
+};
 
 const LINE_WIDTH: i32 = 24;
 
@@ -10,9 +13,22 @@ extern "C" fn Play(
     this: *mut Il2CppObject, mut jikkyou_text: *mut Il2CppString, jikkyou_voice_cmd: *mut Il2CppString,
     type_: i32, tension: i32, on_end: *mut Il2CppObject, is_cross_time_enable: bool
 ) {
-    if let Some(wrapped) = wrap_text_il2cpp(jikkyou_text, LINE_WIDTH) {
+    let text = unsafe { (*jikkyou_text).to_utf16str() };
+
+    if text.as_slice().contains(&36) { // 36 = dollar sign ($)
+        let processed = Hachimi::instance().template_parser.eval(&text.to_string());
+        let final_text = if let Some(wrapped) = wrap_text(&processed, LINE_WIDTH) {
+            wrapped.join("\n")
+        }
+        else {
+            processed
+        };
+        jikkyou_text = final_text.to_il2cpp_string();
+    }
+    else if let Some(wrapped) = wrap_text_il2cpp(jikkyou_text, LINE_WIDTH) {
         jikkyou_text = wrapped;
     }
+
     get_orig_fn!(Play, PlayFn)(this, jikkyou_text, jikkyou_voice_cmd, type_, tension, on_end, is_cross_time_enable);
 }
 
