77cdb046c1feaf1f7e914984bacf588f9f76dfcd
LeadRDRK <leadrdrk@zohomail.com>
2024-08-23 12:53:50 +0700

    Fix skill learning item query flag trigger

--- src/il2cpp/hook/umamusume/PartsSingleModeSkillLearningListItem.rs
+++ src/il2cpp/hook/umamusume/PartsSingleModeSkillLearningListItem.rs
@@ -1,16 +1,16 @@
 use crate::{core::sql::TextDataQuery, il2cpp::{symbols::get_method_addr, types::*}};
 
-type UpdateSkillNameFn = extern "C" fn(this: *mut Il2CppObject);
-extern "C" fn UpdateSkillName(this: *mut Il2CppObject) {
+type UpdateCurrentFn = extern "C" fn(this: *mut Il2CppObject);
+extern "C" fn UpdateCurrent(this: *mut Il2CppObject) {
     TextDataQuery::with_skill_learning_query(|| {
-        get_orig_fn!(UpdateSkillName, UpdateSkillNameFn)(this);
+        get_orig_fn!(UpdateCurrent, UpdateCurrentFn)(this);
     });
 }
 
 pub fn init(umamusume: *const Il2CppImage) {
     get_class_or_return!(umamusume, Gallop, PartsSingleModeSkillLearningListItem);
 
-    let UpdateSkillName_addr = get_method_addr(PartsSingleModeSkillLearningListItem, c"UpdateSkillName", 0);
+    let UpdateCurrent_addr = get_method_addr(PartsSingleModeSkillLearningListItem, c"UpdateCurrent", 0);
 
-    new_hook!(UpdateSkillName_addr, UpdateSkillName);
+    new_hook!(UpdateCurrent_addr, UpdateCurrent);
 }
